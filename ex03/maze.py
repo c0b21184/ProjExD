ee29d6a61d4c6a3ce3b2149b93fad38ad6ed8ba5
taiga <c0b2118485@edu.teu.ac.jp>
2022-06-28 14:37:25 +0900

    問六

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up():
+    global key
+    key = ""
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
 
-    tori = tk.PhotoImage(file="fig/6.png")
+    tori = tk.PhotoImage(file="fig/3.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori")
-    
+
+    key = ""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+
     root.mainloop()
