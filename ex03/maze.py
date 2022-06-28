691e8d65e6d3b6a0c6a123695cf3603273af9fc0
taiga <c0b2118485@edu.teu.ac.jp>
2022-06-28 14:26:43 +0900

    問三

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,4 +3,11 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas.pack()
+
+    tori = tk.PhotoImage(file="fig/6.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+    
     root.mainloop()
